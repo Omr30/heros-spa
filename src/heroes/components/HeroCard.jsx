b1a12420258e8d1fd3ce6f31f340ad58c84f1e4d
mvrk <oscarmenacho23@gmail.com>
2023-04-25 01:25:18 -0500

    fix img prod

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -15,7 +15,7 @@ export const HeroCard = ({
     first_appearance,
     characters,
 }) => {
-    const heroImage = `/assets/heroes/${id}.jpg`
+    const heroImage = `/heroes/${id}.jpg`
 
     return (
         <div className="col animate__animated animate__fadeIn">
